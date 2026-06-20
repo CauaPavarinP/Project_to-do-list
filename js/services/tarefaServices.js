76305350efcf664b1b7b0f448623c1a442b6e097
CauaPavarinP <cauapavarinportolan@gmail.com>
2026-06-20 18:05:17 -0300

    Implementa captura e armazenamento de tarefas em array

--- js/services/tarefaServices.js
+++ js/services/tarefaServices.js
@@ -1,48 +1,45 @@
-//const tarefas = [];
+    const tarefas = [];
 
-//function pegarCampos(){
-    //return{
-    //    inputNomeTarefa = document.querySelector("#nomeTarefa"),
-    //    inputDescricao = document.querySelector("#descricao"),
-    //    inputDataLimite = document.querySelector("#input-dataLimite"),
-    //    inputOpcoes = document.querySelector("#opcoes-prioridades"),
-    //    selectCategoria = document.querySelector("#categoria")
-  //  }
+    function pegarCampos(){
+        return{
+            inputNomeTarefa: document.querySelector("#nomeTarefa"),
+            inputDescricao: document.querySelector("#descricao"),
+            inputDataLimite: document.querySelector("#input-dataLimite"),
+            inputOpcoes: document.querySelector("#opcoes-prioridades"),
+            selectCategoria: document.querySelector("#categoria")
+        }
+    }
 
+    //Função que salva a tarefa e não recarrega a página
+    export function configurarForm(){
 
-//}
+        const campos = pegarCampos();
 
-//Função que salva a tarefa e não recarrega a página
-export function configurarForm(){
+        const form = document.querySelector("#NovaTarefa");
 
-    const form = document.querySelector("#NovaTarefa");
-    const inputNomeTarefa = document.querySelector("#nomeTarefa");
-    const inputDescricao = document.querySelector("#descricao");
-    const inputDataLimite = document.querySelector("#input-dataLimite");
-    const inputOpcoes = document.querySelector("#opcoes-prioridades");
-    const selectCategoria = document.querySelector("#categoria");
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
 
+            const tarefa = {
+                nome: campos.inputNomeTarefa.value,
+                descricao: campos.inputDescricao.value,
+                dataLimite: campos.inputDataLimite.value,
+                //prioridade: campos.inputOpcoes.value
+                categoria: campos.selectCategoria.value
+            }
 
+            tarefas.push(tarefa);
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
+            console.log(tarefas);
+        })
+    }
 
+    //Função que vai salvar a tarefa em uma array
+    //export function AdicionarTarefa(tarefas){
 
-        console.log(inputNomeTarefa.value);
-        console.log(inputDescricao.value);
-        console.log(inputDataLimite.value);
-        //inputOpcoes da erro
-        //console.log(inputOpcoes.value);
-        console.log(selectCategoria.value);
-    })
-}
-
-//Função que vai salvar a tarefa em uma array
-//export function AdicionarTarefa(tarefas){
-
-//    tarefas.push({
-//        nome: inputNomeTarefa.value
-//    });
-//}
+    //    tarefas.push({
+    //        nome: inputNomeTarefa.value
+    //    });
+    //}
 
 
